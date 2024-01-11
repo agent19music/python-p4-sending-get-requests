9faf5d98b3ad0ffe46db5331c8682f30abb5b6d1
agent19music <seanmotanya@gmail.com>
2024-01-11 13:46:42 +0300

    Done

--- lib/get.py
+++ lib/get.py
@@ -5,6 +5,10 @@ url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locatio
 
 response = requests.get(url)
 
-json_content = json.loads(response.text)
+# json_content = json.loads(response.text)
 
-print(json.dumps(json_content, indent=4))
+# print(json.dumps(json_content, indent=4))
+
+json_content = json.loads(response.text)
+print("Type:", type(json_content))
+print("\nData:")
